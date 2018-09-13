e47a53e796f531ed4f94aa8bcb7f68912fac0f73
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-13 12:49:13 +0200

    carShopv2

--- src/carShop2.java
+++ src/carShop2.java
@@ -3,12 +3,21 @@ public class carShop2 {
         Car car1 = new Car("BMW", "M5", 2007, 2.6); //marka, model, rocznik, pojemność silnika
         Car car2=new Car("Opel", "Vectra", 2004,1.8);
 
+    String Br="BMW";
+    String Mo="M5";
+
+
+
+
         System.out.println("Samochód 1:");
-        System.out.println("Marka: " + car1.Brand + ", model: " + car1.Model +
-                ", rocznik: " + car1.Year + ", pojemność: " + car1.Capacity);
-        System.out.println("Samochód 2:");
-        System.out.println("Marka: " + car2.Brand + ", model: " + car2.Model +
-                ", rocznik: " + car2.Year + ", pojemność: " + car2.Capacity);
+        System.out.printf("Marka: %S, Model: %S, Rocznik: %s, Pojemność silnika: %s",Br, Mo, 2007, 2.6);
+
+        String Br2="Opel";
+        String Mo2="Vectra";
+
+
+        System.out.println("\nSamochód 2:");
+        System.out.printf("Marka: %S, Model: %S, Rocznik: %s, Pojemność silnika: %s", Br2, Mo2, 2004, 1.8);
     }
 }
 
